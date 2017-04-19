b45b0c3ccc89b6ff7e62210fdbf8e16355693932
Ashley Brooks <ashley.brooks.a@gmail.com>
2017-04-19 12:14:26 -0700

    Adding Makefile

--- ex1.c
+++ ex1.c
@@ -5,8 +5,9 @@ int main(int argc, char *argv[])
 {
     int distance = 100;
 
-    // this is also a comment
-    printf("You are %d miles away.\n", distance);
+    // \n is important because otherwise the line will end with a % sign
+    printf("You are %d miles away.\n", distance); 
+    printf("HELLO HOW ARE YOU\n");
 
-    return 0;
+    return 0; // The program still works if you delete this line. 
 }
